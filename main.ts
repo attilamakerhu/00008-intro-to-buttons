29e084d0bc57188bb2a96b17fc682fa4ca20cc1a
OolongJohnson <83778844+OolongJohnson@users.noreply.github.com>
2021-07-23 16:19:32 +0200

    .hex uploaded

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
 })
